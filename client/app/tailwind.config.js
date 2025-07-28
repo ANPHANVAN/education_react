c58d4606c2c49d1712e4c725cb70e866c5251c4e
ANPHANVAN <2k1.xam@gmail.com>
2025-07-28 19:06:51 +0700

    config:fix warning frontend, config tailwind and delete postCss but it dont work

--- client/app/tailwind.config.js
+++ client/app/tailwind.config.js
@@ -1,31 +1,31 @@
 module.exports = {
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--color-bg)',
-        content: 'var(--color-text)',
-        header: 'var(--color-header)',
-        muted: 'var(--color-muted)',
-        tertiary: 'var(--color-tertiary)',
-        accent: 'var(--color-accent)',
-        secondary: 'var(--color-secondary)',
-        primary: {
-          from: 'var(--color-primary-from)',
-          to: 'var(--color-primary-to)',
-        },
-        surface: 'var(--color-surface)',
-      },
-      animation: {
-        'fade-in': 'fade-in 0.5s ease-out forwards',
-      },
-      keyframes: {
-        'fade-in': {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
-        },
-      },
-    },
+    // extend: {
+    // colors: {
+    //   background: 'var(--color-bg)',
+    //   content: 'var(--color-text)',
+    //   header: 'var(--color-header)',
+    //   muted: 'var(--color-muted)',
+    //   tertiary: 'var(--color-tertiary)',
+    //   accent: 'var(--color-accent)',
+    //   secondary: 'var(--color-secondary)',
+    //   primary: {
+    //     from: 'var(--color-primary-from)',
+    //     to: 'var(--color-primary-to)',
+    //   },
+    //   surface: 'var(--color-surface)',
+    // },
+    // animation: {
+    //   'fade-in': 'fade-in 0.5s ease-out forwards',
+    // },
+    // keyframes: {
+    //   'fade-in': {
+    //     from: { opacity: 0 },
+    //     to: { opacity: 1 },
+    //   },
+    // },
+    // },
   },
   plugins: [],
 };
